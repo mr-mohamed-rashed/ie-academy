67260ff5ed20ddf31df492ce45fd2c18ac8f7d78
mr-mohamed-rashed <mrrashed0777@gmail.com>
2026-08-27 05:19:51 +0300

    Install and integrate vite-plugin-image-optimizer and svgo to automate image compressions

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      exclude: ['**/*.svg'],
+      webp: {
+        quality: 75,
+      },
+      avif: {
+        quality: 65,
+      },
+      png: {
+        quality: 75,
+      },
+      jpeg: {
+        quality: 75,
+      }
+    })
+  ],
   base: '/ie-academy/',
   build: {
     sourcemap: true,
